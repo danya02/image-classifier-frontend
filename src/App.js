10601147cfbb0ecc0ecfc97e87a100ce8f6f1ef2
Danya Generalov <danya@danya02.ru>
2023-05-17 23:10:48 +0300

    Adding Bootstrap and router.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
+
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import { LinkContainer } from 'react-router-bootstrap';
+
+const Home = () => <><span>Home</span></>;
+
+const About = () => <><span>About</span></>;
+
+const Users = () => <><span>Users</span></>;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Container className="p-3">
+      <Container className="p-5 mb-4 bg-light rounded-3">
+        <h1 className="header">Welcome To React-Bootstrap</h1>
+        <h2>
+          Current Page is{' '}
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/" element={<Home />}/>
+          </Routes>
+        </h2>
+        <h2>
+          Navigate to{' '}
+          <ButtonToolbar className="custom-btn-toolbar">
+            <LinkContainer to="/">
+              <Button>Home</Button>
+            </LinkContainer>
+            <LinkContainer to="/about">
+              <Button>About</Button>
+            </LinkContainer>
+            <LinkContainer to="/users">
+              <Button>Users</Button>
+            </LinkContainer>
+          </ButtonToolbar>
+        </h2>
+      </Container>
+    </Container>
+  </BrowserRouter>
   );
 }
 
